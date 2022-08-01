c9700c404a5fd016941bf3d6b90cab18fe88d6ff
Stefany Mojica <stefymojica40@gmail.com>
2022-08-01 14:52:10 -0500

    css-selectors

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -14,9 +14,6 @@ describe("Buy a t-shirt", () => {
     cy.get(".cart_navigation [type=\"submit\"]").click();
     cy.get(".bankwire").click();
     cy.get(".cart_navigation [type=\"submit\"]").click();
-    cy.get("#center_column > div > p > strong").should(
-        "have.text",
-        "Your order on My Store is complete.",
-    );
+    cy.get("#center_column > div > p > strong").should("have.text", "Your order on My Store is complete.");
   });
 });
