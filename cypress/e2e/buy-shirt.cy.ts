8805f89f4add815f22c59107ea0f633a4e67db63
Stefany Mojica <stefymojica40@gmail.com>
2022-08-01 15:29:03 -0500

    css-selectors

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -3,7 +3,7 @@ describe("Buy a t-shirt", () => {
     cy.visit("http://automationpractice.com/");
     cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
     cy.get(
-        "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
+      "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
     ).click();
     cy.get("[style*='display: block;'] .button-container > a").click();
     cy.get(".cart_navigation span").click();
@@ -11,14 +11,14 @@ describe("Buy a t-shirt", () => {
     cy.get("#email").type("aperdomobo@gmail.com");
     cy.get("#passwd").type("WorkshopProtractor");
     cy.get("#SubmitLogin").click();
-    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get(".cart_navigation [type='submit']").click();
     cy.get("#cgv").click();
-    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get(".cart_navigation [type='submit']").click();
     cy.get(".bankwire").click();
-    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get(".cart_navigation [type='submit']").click();
     cy.get("#center_column > div > p > strong").should(
-        "have.text",
-        "Your order on My Store is complete.",
+      "have.text",
+      "Your order on My Store is complete.",
     );
   });
 });
