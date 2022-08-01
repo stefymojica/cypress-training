b692ffac48c0c88f25bf01c10f7b005a42e3c524
Stefany Mojica <stefymojica40@gmail.com>
2022-08-01 15:14:23 -0500

    test

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -2,7 +2,9 @@ describe("Buy a t-shirt", () => {
   it("then the t-shirt should be bought", () => {
     cy.visit("http://automationpractice.com/");
     cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
-    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
+    cy.get(
+        "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
+    ).click();
     cy.get("[style*='display: block;'] .button-container > a").click();
     cy.get(".cart_navigation span").click();
 
@@ -14,6 +16,9 @@ describe("Buy a t-shirt", () => {
     cy.get(".cart_navigation [type=\"submit\"]").click();
     cy.get(".bankwire").click();
     cy.get(".cart_navigation [type=\"submit\"]").click();
-    cy.get("#center_column > div > p > strong").should("have.text", "Your order on My Store is complete.");
+    cy.get("#center_column > div > p > strong").should(
+        "have.text",
+        "Your order on My Store is complete.",
+    );
   });
 });
