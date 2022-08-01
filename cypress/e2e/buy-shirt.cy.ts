0f529b18d5ac4d104809f6b543fbec952d3a2dd6
Stefany Mojica <stefymojica40@gmail.com>
2022-08-01 14:45:58 -0500

    css-selectors

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -2,9 +2,7 @@ describe("Buy a t-shirt", () => {
   it("then the t-shirt should be bought", () => {
     cy.visit("http://automationpractice.com/");
     cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
-    cy.get(
-        "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
-    ).click();
+    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
     cy.get("[style*='display: block;'] .button-container > a").click();
     cy.get(".cart_navigation span").click();
 
