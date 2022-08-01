2d0e2439c1c5c563642f404dfa7e7a0232395313
Stefany Mojica <stefymojica40@gmail.com>
2022-08-01 14:44:44 -0500

    css-selectors

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -2,16 +2,18 @@ describe("Buy a t-shirt", () => {
   it("then the t-shirt should be bought", () => {
     cy.visit("http://automationpractice.com/");
     cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
-    cy.get(
-        "#center_column a.button.ajax_add_to_cart_button.btn.btn-default",
-    ).click();
+    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
     cy.get("[style*='display: block;'] .button-container > a").click();
     cy.get(".cart_navigation span").click();
 
     cy.get("#email").type("aperdomobo@gmail.com");
     cy.get("#passwd").type("WorkshopProtractor");
     cy.get("#SubmitLogin").click();
-    cy.get("[type=\"submit\"]").click();
+    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get("#cgv").click();
+    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get(".bankwire").click();
+    cy.get(".cart_navigation [type=\"submit\"]").click();
+    cy.get("#center_column > div > p > strong").should("have.text", "Your order on My Store is complete.");
   });
 });
-
